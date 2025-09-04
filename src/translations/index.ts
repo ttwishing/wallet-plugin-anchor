e5cdb46faa8a57db9bedbf147b102bad87b362ea
kurt <ttwishing@gmail.com>
2025-09-04 19:18:39 +0800

    test

--- src/translations/index.ts
+++ src/translations/index.ts
@@ -1,13 +1,11 @@
 import en from './en.json'
 import ko from './ko.json'
-import zh from './zh.json'
 import zh_hans from './zh-hans.json'
 import zh_hant from './zh-hant.json'
 
 export default {
     en,
     ko,
-    zh,
     'zh-Hans': zh_hans,
     'zh-Hant': zh_hant,
 }
